ee447aaa295f828860b207987731715538505ef1
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-16 20:18:43 +0100

    app now renders Game Interface and is able to switch between start Interface and the actual game interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
+import { createTheme , ThemeProvider, makeStyles } from '@material-ui/core/styles'
 import './App.css';
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import { Radio, Button, Grid, Paper, Card, Container } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Components
+
+import CustomBtn from './components/CustomBtn'
+import Input from './components/Input'
+import StartCard from './components/StartCard'
+import Game from './components/Game';
+
+
+// Icons
+
+import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
+import { ThreeSixtySharp } from '@material-ui/icons';
+
+
+const styles = makeStyles({
+  wrapper: {
+    margin: '1rem',
+  },
+  card:{
+    width:'65%', 
+    float:'right',
+    marginRight:'6rem', 
+    padding:'2rem', 
+    paddingBottom : { xs: 3, sm: 3, md: 3, lg : 3 }, 
+  },
+  grid:{
+    display: 'flex', 
+    justifyContent: 'center', 
+    paddingTop : '2rem',
+    paddingBottom : '2rem', 
+  },
+  gridcontainer:{
+    width : '45%', 
+    alignItems : 'center', 
+    float: 'right', 
+    padding : '2rem', 
+  },
+  griditem: {
+    justifyContent: 'center', 
+    display: 'flex',   
+  }, 
+  header: {
+    padding : '6rem'
+  },
+  footer: {
+    padding : '16rem'
+  }, 
+});
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      gameStartButton : false, 
+    }
+
+    this.HandleGameStart = this.HandleGameStart.bind(this)
+    this.RenderGame = this.RenderGame.bind(this)
+    this.GameInterface = this.GameInterface.bind(this)
+
+  }
+
+  HandleGameStart(){
+    this.setState(
+      {gameStartButton : !this.state.gameStartButton}
+    )
+  }
+
+  GameInterface(props){
+
+    if (props.gameStartButton){
+      return(
+        <Game HandleGameStart = {() => {this.HandleGameStart()}}/>
+      )
+    }
+    else{
+      return(
+      <StartCard Click = {() => {this.HandleGameStart()}}/>
+      );
+    }
+    }
+
+  RenderGame(props){
+    const classes = styles()
+      return (
+        <div>
+          <div className = {classes.header}></div>
+          <this.GameInterface gameStartButton = {props.gameStartButton}/> 
+          <div className = {classes.footer}> </div>
+        </div>
+      );
+  }
+
+  render(){
+      return (
+        <this.RenderGame gameStartButton = {this.state.gameStartButton}/>
+        // <Game/>
+      );
+  }
 }
+
 
 export default App;
