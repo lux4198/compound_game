c5191a1d87e0db79b1dafd431353dc20f8885e89
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-17 20:08:47 +0100

    implemented timer function and lifecycle methods

--- src/components/Game.js
+++ src/components/Game.js
@@ -50,35 +50,69 @@ class Game extends React.Component{
             round : {
                 current : 1, 
                 capital : Math.round((Math.random() * 100))*10000, 
-                time : Math.round(Math.random() * 30), 
+                years : Math.round(Math.random() * 30), 
                 interest : Math.round(Math.random() * 12), 
             },
+
+            seconds : 20,
+            intervalID : '',  
+
             userInput : '', 
             submissionInput : '',  
         }
         
+        this.timer = this.timer.bind(this)
         this.GameText = this.GameText.bind(this)
         this.GameCard = this.GameCard.bind(this)
         this.HandleSubmit = this.HandleSubmit.bind(this)
 
     };
 
+    componentDidMount(){
+        var IntervalId = setInterval(this.timer, 1000)
+        // store intervalId in the state so it can be accessed later:
+        this.setState({intervalId : IntervalId})
+        }
+     
+    componentWillUnmount() {
+        // use intervalId from the state to clear the interval
+        clearInterval(this.state.intervalId)
+        this.setState({userInput : this.state.submissionInput, 
+            round : {
+                current : this.state.round.current += 1, 
+                capital : Math.round(((Math.random() + 0.3) * 10))*10000, 
+                years : Math.round(Math.random() * 25) + 1, 
+                interest : Math.round(Math.random() * 15) + 1,
+            },
+            seconds : 20,
+            submissionInput : ''})
+     }
+
+    timer() {
+        if (this.state.seconds > 0){
+            this.setState( { seconds : this.state.seconds -= 1})
+        } else {
+            this.componentWillUnmount()
+            this.componentDidMount()
+        }
+    }
+
     GameText() {
         const classes = styles()
         return(
     
         <div className = {classes.text}>
-            <Typography variant = 'h4' style = {{paddingBottom: '2rem',}}> Example No. {this.state.round.current} </Typography>
+            <Typography variant = 'h4' style = {{paddingBottom: '2rem',}}> Example No. {this.state.round.current} - {this.state.seconds}</Typography>
             <Typography variant = 'h6' style = {{}}> 
             You have {this.state.round.capital} $ in the bank today. <br></br> 
             Your bank will provide you with {this.state.round.interest} % interest per Year. <br></br>
-            How much Money will you have after {this.state.round.time} Years? 
+            How much Money will you have after {this.state.round.years} Years? 
             <br></br>
             Good Luck! 
             </Typography>
         </div>
         )
-    };
+    }
 
     Submitbutton(props){
         return(
@@ -94,13 +128,8 @@ class Game extends React.Component{
         e.preventDefault()
         const re = /^[0-9]+$/ 
         if (re.test(this.state.submissionInput)) {
-            this.setState({userInput : this.state.submissionInput, 
-                            round : {
-                                current : this.state.round.current += 1, 
-                                capital : Math.round(((Math.random() + 0.3) * 10))*10000, 
-                                time : Math.round(Math.random() * 25) + 1, 
-                                interest : Math.round(Math.random() * 15) + 1, 
-                            }})
+            this.componentWillUnmount()
+            this.componentDidMount()
             }
         else {
             alert('Please type in a whole number')
@@ -116,7 +145,8 @@ class Game extends React.Component{
                     <this.GameText/>
                     <EndGameButton Click = {() => {props.HandleGameStart()}}/>
                     <form /* noValidate */ autoComplete = 'off' onSubmit = {this.HandleSubmit}>
-                        <Input style = {{padding : '2rem', }} onChange = {(e) => {this.setState({submissionInput : e.target.value})}}
+                        <Input style = {{padding : '2rem', }}  value = {this.state.submissionInput} 
+                                onChange = {(e) => {this.setState({submissionInput : e.target.value})}}
                                 />
                         <this.Submitbutton/>
                     </form>
