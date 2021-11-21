06987628cd4ff5af920d61959397315de90096c9
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-21 11:49:08 +0100

    button displays different hover colors for primary and secondary button -> now works with customized hover colors

--- src/components/CustomBtn.js
+++ src/components/CustomBtn.js
@@ -9,17 +9,17 @@ const styles = makeStyles({
           backgroundColor : '#9B3344', 
         }, 
       },
-      secondarybutton:{
-        '&:hover': {
-          backgroundColor : '#2E5D68', 
-        }, 
-      }
-    });
+    secondarybutton:{
+      '&:hover': {
+        backgroundColor : '#2E5D68',  
+      }, 
+    }
+  });
 
 function CustomBtn(props) {
     const classes = styles()
     return (
-        <Button className = {classes.button}
+        <Button className = {(props.color === 'primary')? classes.primarybutton : classes.secondarybutton}
                 variant = 'contained'
                 onClick = {props.Click} 
                 color = {props.color}
