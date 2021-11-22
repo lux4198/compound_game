7d90b23e9eb364948484b86cf220d5fa5071ef76
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-22 23:37:13 +0100

    logo on navbar is now a link and will be linked to home screen once routing is implemented

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Toolbar, Typography, makeStyles } from '@material-ui/core'
+import { Toolbar, Typography, makeStyles, Tooltip } from '@material-ui/core'
 import React from 'react'   
 
 import start_card_expo from '../images/start_card_expo.png';
@@ -6,17 +6,21 @@ import CustomBtn from './CustomBtn';
 
 
 const styles = makeStyles({
-    bar : {
-        backgroundColor : '#526268', 
+    header: {
+        backgroundColor : '#526268',
+    },
+    bar : { 
         ['@media (max-width: 780px)'] : {
             flexdirection : 'column'
-        }
+        },
+        width : '60%'
     }, 
-    logo: {
-        width: "5%", 
+    homeButton: {
+        width: "10%", 
         ['@media (max-width:780px)']: { 
            display: "none"
-           },
+           }, 
+        paddingTop : '0.3rem'
     },
     menuItem: {
         flexGrow : 1, 
@@ -32,36 +36,52 @@ const styles = makeStyles({
     }
 })
 
+const aboutTooltip = () => {
+    return(
+        <Typography variant = 'h5'>
+            This is a beginner React project to show my many react skills. <br></br>
+            If You have any Feedback feel free to share it with me via email.
+            Thank you very much for visiting the website and I hope to see you soon. 
+        </Typography>
+    )
+}
+
+const feedbackTooltip = () => {
+    return(
+        <Typography variant = 'h5'>
+            I hope you were able to improve Your compounding knowledge and intuition.<br></br>
+            If you have any feedback or ideas You want to share, feel free to contact us via email. 
+            See You soon. 
+        </Typography>
+    )
+}
+
 function NavBar() {
     const classes = styles()
     return (
-        <Toolbar position = 'sticky' color = 'primary' className = {classes.bar}>
-            <img src = {start_card_expo} className = {classes.logo} style = {{padding : '1rem'}}></img>
+        <div className = {classes.header}>
+            <Toolbar position = 'sticky' color = 'primary' className = {classes.bar}>
+                <a href = 'https://youtube.com' className = {classes.homeButton}>
+                    <img src = {start_card_expo} style = {{width : '99%'}}></img>
+                </a>
 
-            <Typography variant = 'h5' className = {classes.menuItem} style = {{paddingLeft : '4rem'}}>
-                <div className = {classes.menuText}>
-                    About
-                </div>
-            </Typography>
+                <Typography variant = 'h5' className = {classes.menuItem} style = {{paddingLeft : '4rem'}}>
+                    <Tooltip title = {aboutTooltip()}>
+                        <div className = {classes.menuText}>
+                            About 
+                        </div>
+                    </Tooltip>
+                </Typography>
 
-            <Typography variant = 'h5' className = {classes.menuItem}>
-                <div className = {classes.menuText}>
-                    Career 
-                </div>
-            </Typography>
-
-            <Typography variant = 'h5' className = {classes.menuItem}>
-                <div className = {classes.menuText}>
-                    Locations
-                </div>
-            </Typography>
-
-            <Typography variant = 'h5' className = {classes.menuItem}>
-                <div className = {classes.menuText}>
-                    Feedback
-                </div>
-            </Typography>
-        </Toolbar>
+                <Typography variant = 'h5' className = {classes.menuItem}>
+                    <Tooltip title = {feedbackTooltip()}>
+                        <div className = {classes.menuText}>
+                            Feedback
+                        </div>
+                    </Tooltip>
+                </Typography>
+            </Toolbar>
+        </div>
     )
 }
 
